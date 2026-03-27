b031cda3f6db62e28126f28c8a2cd9741f817503
seshasainunna123-dot <seshasai.nunna123@gmail.com>
2026-03-27 18:54:57 +0530

    bild error

--- react-native.config.js
+++ react-native.config.js
@@ -3,11 +3,4 @@ module.exports = {
     ios: {},
     android: {},
   },
-  dependencies: {
-    'react-native-vector-icons': {
-      platforms: {
-        android: null, // disable Android autolinking
-      },
-    },
-  },
 };
